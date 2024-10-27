1a0d4b54541d3dcda3b11dcd860dcfc2db441524
Abasiodiong Udofia <enalsy22@gmail.com>
2024-10-27 17:22:37 +0100

    sql task 2

---
+++ task_2.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Authors (
+  author_id INT AUTO_INCREMENT,
+  author_name VARCHAR(215) NOT NULL,
+  PRIMARY KEY (author_id)
+);
+
+CREATE TABLE Books (
+  book_id INT AUTO_INCREMENT,
+  title VARCHAR(130) NOT NULL,
+  author_id INT,
+  price DOUBLE NOT NULL,
+  publication_date DATE NOT NULL,
+  PRIMARY KEY (book_id),
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Customers (
+  customer_id INT AUTO_INCREMENT,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215) UNIQUE NOT NULL,
+  address TEXT,
+  PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE Orders (
+  order_id INT AUTO_INCREMENT,
+  customer_id INT,
+  order_date DATE NOT NULL,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+  orderdetaildid INT AUTO_INCREMENT,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE NOT NULL,
+  PRIMARY KEY (orderdetaildid),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
